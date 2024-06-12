3eb7462bf5e113a2739f02bdec700c91cf9c4832
Vandit Shah <vanditshah555@gmail.com>
2024-06-12 19:21:28 +0530

    "Added reactive form"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Address-Management-System';
+export class AppComponent implements OnInit {
+  form!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      userFields: this.fb.group({
+        userId: ['', Validators.required],
+        userName: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+      }),
+      addressFields: this.fb.array([
+        this.createAddressGroup()
+      ])
+    });
+  }
+
+  createAddressGroup(): FormGroup {
+    return this.fb.group({
+      street: ['', Validators.required],
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      zipCode: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(10)]]
+    });
+  }
+
+  get addressFields(): FormArray {
+    return this.form.get('addressFields') as FormArray;
+  }
+
+  addAddress(): void {
+    this.addressFields.push(this.createAddressGroup());
+  }
+
+  removeAddress(index: number): void {
+    this.addressFields.removeAt(index);
+  }
+
+  onSubmit(): void {
+    console.log(this.form.value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { UserManagerComponent } from './user-manager/user-manager.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserManagerComponent
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    FormsModule,
+    HttpClientModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
